6f3bea2949ac9dfb89ef452a23e38adb0d8b6a36
Argo Nurcahyo <argonurcahyo25@gmail.com>
2023-05-16 12:13:45 +0700

    done

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,11 +1,11 @@
-class Player {
-    constructor() {
+class Player extends Sprite {
+    constructor({ collisionBlocks = [], imageSrc, frameRate, animations, loop }) {
+        super({ imageSrc, frameRate, animations, loop })
         this.position = {
-            x: 100,
-            y: 100
+            x: 200,
+            y: 200
         }
-        this.width = 100
-        this.height = 100
+
         this.sides = {
             bottom: this.position.y + this.height
         }
@@ -14,20 +14,125 @@ class Player {
             y: 0
         }
         this.gravity = 1
-    }
-    draw() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        this.collisionBlocks = collisionBlocks
     }
 
+    // draw() {
+    //     c.fillStyle = 'red'
+    //     c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    // }
+
     update() {
+        // c.fillStyle = 'rgba(0,0,255,.5)'
+        // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
         this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-        this.sides.bottom = this.position.y + this.height
-        if (this.sides.bottom + this.velocity.y < canvas.height) {
-            this.velocity.y += 1
+        this.updateHitbox()
+
+        this.checkForHorizontalCollision()
+        this.applyGravity()
+
+        this.updateHitbox()
+
+        // c.fillRect(
+        //     this.hitbox.position.x,
+        //     this.hitbox.position.y,
+        //     this.hitbox.width,
+        //     this.hitbox.height
+        // )
+
+        this.checkForVerticalCollision()
+    }
+
+    handleInput(keys) {
+        if (this.preventInput) return
+        this.velocity.x = 0
+        if (keys.d.pressed) {
+            this.switchSprite('runRight')
+            this.velocity.x = 5
+            this.lastDirection = 'right'
+        } else if (keys.a.pressed) {
+            this.switchSprite('runLeft')
+            this.velocity.x = -5
+            this.lastDirection = 'left'
         } else {
-            this.velocity.y = 0
+            if (this.lastDirection === 'left') this.switchSprite('idleLeft')
+            else this.switchSprite('idleRight')
+        }
+    }
+
+    switchSprite(name) {
+        if (this.image === this.animations[name].image) return
+        this.currentFrame = 0
+        this.image = this.animations[name].image
+        this.frameRate = this.animations[name].frameRate
+        this.frameBuffer = this.animations[name].frameBuffer
+        this.loop = this.animations[name].loop
+        this.currentAnimation = this.animations[name]
+    }
+
+    applyGravity() {
+        this.velocity.y += this.gravity
+        this.position.y += this.velocity.y
+    }
+
+    updateHitbox() {
+        this.hitbox = {
+            position: {
+                x: this.position.x + 58,
+                y: this.position.y + 34
+            },
+            width: 50,
+            height: 53
+        }
+    }
+
+    checkForVerticalCollision() {
+        for (let i = 0; i < this.collisionBlocks.length; i++) {
+            const collisionBlock = this.collisionBlocks[i]
+
+            if (this.hitbox.position.x <= collisionBlock.position.x + collisionBlock.width &&
+                this.hitbox.position.x + this.hitbox.width >= collisionBlock.position.x &&
+                this.hitbox.position.y + this.hitbox.height >= collisionBlock.position.y &&
+                this.hitbox.position.y <= collisionBlock.position.y + collisionBlock.height) {
+                if (this.velocity.y < 0) {
+                    this.velocity.y = 0
+                    const offset = this.hitbox.position.y - this.position.y
+                    this.position.y = collisionBlock.position.y + collisionBlock.height - offset + .01
+                    break
+                }
+
+                if (this.velocity.y > 0) {
+                    this.velocity.y = 0
+                    const offset = this.hitbox.position.y - this.position.y + this.hitbox.height
+                    this.position.y = collisionBlock.position.y - offset - .01
+                    break
+                }
+            }
+        }
+    }
+
+    checkForHorizontalCollision() {
+        for (let i = 0; i < this.collisionBlocks.length; i++) {
+            const collisionBlock = this.collisionBlocks[i]
+
+            if (this.hitbox.position.x <= collisionBlock.position.x + collisionBlock.width &&
+                this.hitbox.position.x + this.hitbox.width >= collisionBlock.position.x &&
+                this.hitbox.position.y + this.hitbox.height >= collisionBlock.position.y &&
+                this.hitbox.position.y <= collisionBlock.position.y + collisionBlock.height) {
+                if (this.velocity.x < 0) {
+                    const offset = this.hitbox.position.x - this.position.x
+                    this.position.x = collisionBlock.position.x + collisionBlock.width - offset + .01
+                    break
+                }
+
+                if (this.velocity.x > 0) {
+                    const offset = this.hitbox.position.x - this.position.x + this.hitbox.width
+                    this.position.x = collisionBlock.position.x - offset - .01
+                    break
+                }
+            }
         }
     }
 }
